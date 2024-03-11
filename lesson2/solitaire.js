beeadf48829594e70ef851552ecab5157e39046b
John Lafer <jlafer@yahoo.com>
2024-03-11 14:09:48 -0700

    a little cleanup

--- lesson2/solitaire.js
+++ lesson2/solitaire.js
@@ -6,45 +6,40 @@ import readline from 'readline/promises';
  * - playerCards: the cards that the player can draw from; the top card is at the end of the array
  * - discardPile: the cards that the player has drawn from playerCards; the top card is at the end of the array
  *   when the player picks up the discard pile, the cards are added to playerCards in reverse order
- * - columns: the starting cards that are dealt to the board; the top card of each column is at the end of each array
+ * - boardColumns: the starting cards that are dealt to the board; the top card of each column is at the end of each array
  * - suitColumns: the cards that are moved to the suit columns; the top card of each column is at the end of each array
  */
-const game = {
-  deck: getDeck(),
-  columns: [],
-  suitColumns: {},
-  playerCards: [],
-  discardPile: [],
-};
 
 function dealTheCards(game) {
   shuffleDeck(game.deck);
   game.gameOver = false;
   game.suitColumns = {  hearts: [], diamonds: [], clubs: [], spades: [] };
-  game.columns = [];
-  for (let i = 0; i < 7; i++) {
-    game.columns.push([]);
-  }
+  game.boardColumns = makeBoardColumns(game.deck);
+  game.playerCards = game.deck;
+  game.discardPile = [];
+}
+
+function makeBoardColumns(deck) {
+  const boardColumns = [[], [], [], [], [], [], []];
   let boardColumn = 0;
   let firstColumnToFill = 0;
   for (let i = 0; i < 28; i++) {
-    game.deck[0].faceUp = (boardColumn === firstColumnToFill);
-    game.columns[boardColumn].push(game.deck.shift());
+    deck[0].faceUp = (boardColumn === firstColumnToFill);
+    boardColumns[boardColumn].push(deck.shift());
     boardColumn++;
     if (boardColumn === 7) {
       firstColumnToFill++;
       boardColumn = firstColumnToFill;
     }
   }
-  game.playerCards = game.deck;
-  game.discardPile = [];
+  return boardColumns;
 }
 
 function showGameState(game) {
   console.log('');
   showSuitColumns(game);
   console.log(`Board cards:`);
-  game.columns.forEach((column, index) => {
+  game.boardColumns.forEach((column, index) => {
     const columnString = column.map(card => card.faceUp ? getCardString(card) : 'XX').join(', ');
     console.log(`Column ${index + 1}: ${columnString}`);
   });
@@ -133,10 +128,8 @@ async function getPlayerMove(game) {
 
 function moveDiscardToSuitColumn(game) {
   const fromCard = game.discardPile.pop();
-  const {suit, rank} = fromCard;
-  const suitColumn = game.suitColumns[suit.name];
-  if (suitColumn.length === 0 && rank.name === 'A'
-    || suitColumn.length > 0 && suitColumn[suitColumn.length - 1].rank.value === rank.value - 1) {
+  const suitColumn = game.suitColumns[fromCard.suit.name];
+  if (cardCanGoOnSuitColumn(suitColumn, fromCard)) {
     suitColumn.push(fromCard);
   } else {
     game.discardPile.push(fromCard);
@@ -148,14 +141,10 @@ function moveDiscardToSuitColumn(game) {
 }
 
 function moveBoardCardToSuitColumn(game, fromColNum) {
-  const fromBoardColumn = game.columns[fromColNum - 1];
+  const fromBoardColumn = game.boardColumns[fromColNum - 1];
   const fromCard = fromBoardColumn.pop();
-  console.log(`moving card ${getCardString(fromCard)} from column ${fromColNum}`);
-  const {suit, rank} = fromCard;
-  const suitColumn = game.suitColumns[suit.name];
-  console.log(`to suit column ${suit.name} with ${suitColumn.length} cards`);
-  if (suitColumn.length === 0 && rank.name === 'A'
-    || suitColumn.length > 0 && suitColumn[suitColumn.length - 1].rank.value === rank.value - 1) {
+  const suitColumn = game.suitColumns[fromCard.suit.name];
+  if (cardCanGoOnSuitColumn(suitColumn, fromCard)) {
     suitColumn.push(fromCard);
     turnOverTopBoardCard(fromBoardColumn);
   }
@@ -170,21 +159,21 @@ function moveBoardCardToSuitColumn(game, fromColNum) {
 }
 
 function moveBoardColumn(game, fromColNum, toColNum) {
-  const fromBoardColumn = game.columns[fromColNum - 1];
-  const toBoardColumn = game.columns[toColNum - 1];
+  const fromBoardColumn = game.boardColumns[fromColNum - 1];
+  const toBoardColumn = game.boardColumns[toColNum - 1];
   const bottomFromCardIdx = fromBoardColumn.findIndex(card => card.faceUp);
   const bottomFromCard = fromBoardColumn[bottomFromCardIdx];
   const toCard = toBoardColumn[toBoardColumn.length - 1];
   if (toBoardColumn.length === 0 && bottomFromCard.rank.name === 'K'
     || toBoardColumn.length > 0 && cardCanGoOnBoardCard(toCard, bottomFromCard)) {
-    game.columns[toColNum - 1] = toBoardColumn.concat(fromBoardColumn.splice(bottomFromCardIdx));
+    game.boardColumns[toColNum - 1] = toBoardColumn.concat(fromBoardColumn.splice(bottomFromCardIdx));
     turnOverTopBoardCard(fromBoardColumn);
   }
 }
 
 function moveDiscardToBoardColumn(game, toColNum) {
   const fromCard = game.discardPile.pop();
-  const toBoardColumn = game.columns[toColNum - 1];
+  const toBoardColumn = game.boardColumns[toColNum - 1];
   const topBoardCard = toBoardColumn[toBoardColumn.length - 1];
   if (toBoardColumn.length === 0 && fromCard.rank.name === 'K'
     || toBoardColumn.length > 0 && cardCanGoOnBoardCard(topBoardCard, fromCard)) {
@@ -224,13 +213,15 @@ function cardCanGoOnBoardCard(boardCard, card) {
   return boardCard.suit.color !== card.suit.color
     && boardCard.rank.value === card.rank.value + 1;
 }
+
+function cardCanGoOnSuitColumn(suitColumn, card) {
+  return suitColumn.length === 0 && card.rank.name === 'A'
+    || suitColumn.length > 0 && suitColumn[suitColumn.length - 1].rank.value === card.rank.value - 1;
+}
+
 async function playOneGame() {
   const game = {
-    deck: getDeck(),
-    columns: [],
-    suitColumns: [],
-    playerCards: [],
-    discardPile: [],
+    deck: getDeck()
   };
   dealTheCards(game);
   while (!game.gameOver) {
